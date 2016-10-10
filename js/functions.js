d29bfce4a1c66033e846ff61c86e9db4e2080020
NVS Abhilash <nvs232@gmail.com>
2016-10-10 09:51:43 +0530

    Simplified code and added responsiveness

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+function change(){
+  document.getElementById("name").innerHTML = "''$!d....@r+''";
+}
+
+function back(){
+  document.getElementById("name").innerHTML = "SIDDHARTH SHASHIKAR";
+  document.getElementById("name").style.marginLeft="0";
+}
